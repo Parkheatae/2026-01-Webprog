23094fb965d1f184793dbb555409ac4b4285fd1e
Parkheetae <hite0330@naver.com>
2026-05-28 22:57:59 +0900

    13주차

--- 20210596/week13/Product_Rest/src/main/java/kr/hnu/ice/productrest/ProductController.java
+++ 20210596/week13/Product_Rest/src/main/java/kr/hnu/ice/productrest/ProductController.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/products")
 public class ProductController extends HttpServlet {
     private ProductService service;
 
@@ -20,11 +22,12 @@ public class ProductController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+
         String action = request.getParameter("action");
         String view;
 
-        if ("info".
-                equals(action)) {
+        if ("info".equals(action)) {
             view = info(request, response);
         } else {
             view = list(request, response);
