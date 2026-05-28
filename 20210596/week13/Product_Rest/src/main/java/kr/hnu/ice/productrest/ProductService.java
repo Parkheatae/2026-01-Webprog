ab888f9b99fcbd82f34ebf198d52fe13dedf23d3
Parkheetae <hite0330@naver.com>
2026-05-28 22:55:42 +0900

    13주차

--- 20210596/week13/Product_Rest/src/main/java/kr/hnu/ice/productrest/ProductService.java
+++ 20210596/week13/Product_Rest/src/main/java/kr/hnu/ice/productrest/ProductService.java
@@ -6,9 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 public class ProductService {
-    private Map<String, Product> products = new LinkedHashMap<String, Product>();
+    private static Map<String, Product> products = new LinkedHashMap<String, Product>();
 
-    public ProductService() {
+    static {
         products.put("P001", new Product("P001", "노트북", "LG", 1200000, "2026-05-06"));
         products.put("P002", new Product("P002", "스마트폰", "Samsung", 850000, "2026-04-15"));
         products.put("P003", new Product("P003", "무선 이어폰", "Sony", 220000, "2026-03-20"));
@@ -22,6 +22,15 @@ public class ProductService {
         if (id == null) {
             return null;
         }
+
         return products.get(id);
+    }
+
+    public boolean delete(String id) {
+        if (id == null) {
+            return false;
+        }
+
+        return products.remove(id) != null;
     }
 }
